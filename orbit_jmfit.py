c19d9554dd18356e52b51c4c620764cba7f3faa3
lucasjord <lucas_hyland@hotmail.com>
2021-07-22 13:39:13 +1000

    replaced all single quotes with double quotes

--- orbit_jmfit.py
+++ orbit_jmfit.py
@@ -25,29 +25,29 @@ def main():
     parser.add_argument("-v", "--verbose",
                         help="increase output verbosity",
                         action="count",default=0)
-    parser.add_argument('experiment',
-                        help='experiment code e.g. X420A. Case sensitive.',
+    parser.add_argument("experiment",
+                        help="experiment code e.g. X420A. Case sensitive.",
                         type=str)
-    parser.add_argument('aipsid',
-                        help='AIPS USERID e.g. 666. Between 1-46656',
+    parser.add_argument("aipsid",
+                        help="AIPS USERID e.g. 666. Between 1-46656",
                         type=int)
-    parser.add_argument('-g','--gain',
-                        help='CLEAN loop gain, 0-1',
+    parser.add_argument("-g","--gain",
+                        help="CLEAN loop gain, 0-1",
                         type=float,default=0.3)
-    parser.add_argument('-n','--niter'
-                        help='Number of CLEAN iterations',
+    parser.add_argument("-n","--niter"
+                        help="Number of CLEAN iterations",
                         type=int,default=200)
-    parser.add_argument('-c','--cell'
-                        help='Pixel size in image grid, units arcsec',
+    parser.add_argument("-c","--cell"
+                        help="Pixel size in image grid, units arcsec",
                         type=float,default=0.0001)
-    parser.add_argument('-i','--imsize'
-                        help='Image size in pixels',
+    parser.add_argument("-i","--imsize"
+                        help="Image size in pixels",
                         type=int,default=1024)
-    parser.add_argument('-q','--seq'
-                        help='Sequence of created images. Default 99. Will overwrite <seq> images if they exist.',
+    parser.add_argument("-q","--seq"
+                        help="Sequence of created images. Default 99. Will overwrite <seq> images if they exist.",
                         type=int,default=99)
-    parser.add_argument('-z','--nozap'
-                        help='Zap beam files? Enable to NOT delte the beams.',
+    parser.add_argument("-z","--nozap"
+                        help="Zap beam files? Enable to NOT delte the beams.",
                         type=count,default=0)
 
     args = parser.parse_args()
@@ -59,53 +59,53 @@ def main():
         First check whether experiment exists in AIPS at given AIPS USER#
     '''
     success = 0
-    for ftype in ['_G','_C','_L']
-        for expclass in ['UVDATA']:
+    for ftype in ["_G","_C","_L"]
+        for expclass in ["UVDATA"]:
             for disk in range(3):
                 for seq in range(3):
                     indata = AIPSUVData(args.experiment+ftype,expclass,disk,seq)
                     if indata.exists(): 
-                        #if arg.verbosity>0: print(f'Found file {args.experiment+ftype}.{expclass}.{disk}.{seq}')
+                        #if arg.verbosity>0: print(f"Found file {args.experiment+ftype}.{expclass}.{disk}.{seq}")
                         success = success + 1
                         break
                     else: 
-                        #if arg.verbosity>1: print(f'Did not find file {args.experiment+ftype}.{expclass}.{disk}.{seq}')
+                        #if arg.verbosity>1: print(f"Did not find file {args.experiment+ftype}.{expclass}.{disk}.{seq}")
                         continue
-    if success<=0: sys.exit('No catalogues with INNAME {} in AIPSid {}!'.format(args.experiment,args.aipsid))
+    if success<=0: sys.exit("No catalogues with INNAME {} in AIPSid {}!".format(args.experiment,args.aipsid))
     else: 
-        if arg.verbosity>0: print('Found files in {}, continuing...'.format(args.aipsid))
+        if arg.verbosity>0: print("Found files in {}, continuing...".format(args.aipsid))
         pass
     
 
     '''
         Check whether data has been calibrated sufficiently
     '''
-    if AIPSUVData(args.experiment+'_L','UVDATA',1,2).exists():
-        if arg.verbosity>0: print('Found file {}.'UVDATA'.1.2'.format(args.experiment+'_L'))
-        indata = AIPSUVData(args.experiment+'_L','UVDATA',1,2)
+    if AIPSUVData(args.experiment+"_L","UVDATA",1,2).exists():
+        if arg.verbosity>0: print("Found file {}."UVDATA".1.2".format(args.experiment+"_L"))
+        indata = AIPSUVData(args.experiment+"_L","UVDATA",1,2)
 
-    elif AIPSUVData(args.experiment+'_L','UVDATA',2,2).exists():
-        if arg.verbosity>0: print('Found file {}.'UVDATA'.2.2'.format(args.experiment+'_L'))
-        indata = AIPSUVData(args.experiment+'_L','UVDATA',2,2)
+    elif AIPSUVData(args.experiment+"_L","UVDATA",2,2).exists():
+        if arg.verbosity>0: print("Found file {}."UVDATA".2.2".format(args.experiment+"_L"))
+        indata = AIPSUVData(args.experiment+"_L","UVDATA",2,2)
 
-    else: sys.exit("Cannot find CVELed data {}.'UVDATA'.*.2".format(args.experiment+'_L'))
+    else: sys.exit("Cannot find CVELed data {}."UVDATA".*.2".format(args.experiment+"_L"))
 
     try:
         check_sncl(indata,8,4)
     except RuntimeError:
-        sys.exit('Data is not calibrated sufficiently for this step CL>=8, SN>=4 required')
+        sys.exit("Data is not calibrated sufficiently for this step CL>=8, SN>=4 required")
 
 
     '''
         Now we need to identify matching source names. Will assume J=calibrator and G=target
     '''
     calibrators = [s for s in indata.sources if "J" in s]
-    if arg.verbosity>0: print('Found {} calibrators.'.format(len(calibrators)))
-    if arg.verbosity>1: print('Calibrators: {}'.format(calibrators))
+    if arg.verbosity>0: print("Found {} calibrators.".format(len(calibrators)))
+    if arg.verbosity>1: print("Calibrators: {}".format(calibrators))
     targets     = [s for s in indata.sources if "G" in s]
-    if len(targets)>1: sys.exit('More than one possible target found, targets: {}'.format(targets))
+    if len(targets)>1: sys.exit("More than one possible target found, targets: {}".format(targets))
     target     = targets[-1]
-    if arg.verbosity>0: print('Found target {}'.format(target))
+    if arg.verbosity>0: print("Found target {}".format(target))
     
 
     '''
@@ -113,20 +113,20 @@ def main():
     '''
     cdata = []   
     for i in range(len(calibrators)):
-        if AIPSUVData(calibrators[i],'SPLIT',1,1).exists():
-            #if arg.verbosity>0: print('Found file {}.SPLIT.1.1'.format(calibrators[i]))
-            cdata.append(AIPSUVData(calibrators[i],'SPLIT',1,1))
+        if AIPSUVData(calibrators[i],"SPLIT",1,1).exists():
+            #if arg.verbosity>0: print("Found file {}.SPLIT.1.1".format(calibrators[i]))
+            cdata.append(AIPSUVData(calibrators[i],"SPLIT",1,1))
         else:
-            print('Could not find catalogue {}.SPLIT.1.1'.format(calibrators[i]))
+            print("Could not find catalogue {}.SPLIT.1.1".format(calibrators[i]))
 
 
     '''
         For each valid split file, we are going to image it.
     '''
     for cal_split in cdata:
-        if AIPSImage(cal_split.name,'ILC001',1,args.seq).exists():
-            if arg.verbosity>0: print('Deleting old image {}.ILC001.1.{}'.format(cal_split.name,args.seq))
-            AIPSImage(cal_split.name,'ILC001',1,args.seq).zap()
+        if AIPSImage(cal_split.name,"ILC001",1,args.seq).exists():
+            if arg.verbosity>0: print("Deleting old image {}.ILC001.1.{}".format(cal_split.name,args.seq))
+            AIPSImage(cal_split.name,"ILC001",1,args.seq).zap()
         _image(cal_split,args.gain,args.niter,args.cell,args.imsize,args.seq)
         _zapbeam(cal_split.name,args.seq)
     
@@ -143,7 +143,7 @@ def main():
     i=-1
     for cal_split in cdata:
         i=i+1
-        if AIPSImage(cal_split.name,'ICL001',1,args.seq).exists(): wim = WAIPSImage(cal_split.name,'ICL001',1,args.seq)
+        if AIPSImage(cal_split.name,"ICL001",1,args.seq).exists(): wim = WAIPSImage(cal_split.name,"ICL001",1,args.seq)
         else: continue
         wim.squeeze()
         rms = 3*wim.pixels.std()
@@ -161,8 +161,8 @@ def main():
     x_off2 = (x*snr**2).sum()/((snr**2).sum())
     y_off2 = (y*snr**2).sum()/((snr**2).sum())
 
-    print('Shift is ra={0:+6.4f}, dec={1:+6.4f}'.format(-x_off2, -y_off2))
-    print('Add shift as given to maser pos_shift')
+    print("Shift is ra={0:+6.4f}, dec={1:+6.4f}".format(-x_off2, -y_off2))
+    print("Add shift as given to maser pos_shift")
 
 
 ###################################################
@@ -170,7 +170,7 @@ def main():
 ###################################################
 
 def _image(indata,niter=200,gain=0.3,cell=1.0e-4,imsize=1024,seq=99):
-    imagr            = AIPSTask('imagr')
+    imagr            = AIPSTask("imagr")
     imagr.default
     imagr.indata     = indata
     imagr.docalib    = -1
@@ -184,27 +184,27 @@ def _image(indata,niter=200,gain=0.3,cell=1.0e-4,imsize=1024,seq=99):
     imagr()
 
 def _zapbeam(inname,seq=99):
-    beam=AIPSImage(inname,'IBM001',1,seq)
+    beam=AIPSImage(inname,"IBM001",1,seq)
     if beam.exists():
         beam.zap()
 
 def check_sncl(indata,sn,cl):
-    if (indata.table_highver('AIPS CL')>=cl and
-        indata.table_highver('AIPS SN')>=sn):
-            print('Tables are okay')
+    if (indata.table_highver("AIPS CL")>=cl and
+        indata.table_highver("AIPS SN")>=sn):
+            print("Tables are okay")
     
-    if indata.table_highver('AIPS CL')<cl:
-        print(indata.table_highver('AIPS CL'),cl)
-        raise RuntimeError('Not enough CL tables')
+    if indata.table_highver("AIPS CL")<cl:
+        print(indata.table_highver("AIPS CL"),cl)
+        raise RuntimeError("Not enough CL tables")
 
-    if indata.table_highver('AIPS SN')<sn:
-        print(indata.table_highver('AIPS SN'),sn)
-        raise RuntimeError('Not enough SN tables')
+    if indata.table_highver("AIPS SN")<sn:
+        print(indata.table_highver("AIPS SN"),sn)
+        raise RuntimeError("Not enough SN tables")
 
-def _jmfit(inname='',inseq=1,fitout=''):
-    jmfit           =  AIPSTask('jmfit')
+def _jmfit(inname=",inseq=1,fitout="):
+    jmfit           =  AIPSTask("jmfit")
     jmfit.default
-    image           =  AIPSImage(inname,'ICL001',EXPDISK,inseq)
+    image           =  AIPSImage(inname,"ICL001",EXPDISK,inseq)
     jmfit.indata    =  image
     jmfit.fitout    =  fitout
     jmfit.doprint   = -4
@@ -214,25 +214,25 @@ def _jmfit(inname='',inseq=1,fitout=''):
     if image.exists(): jmfit()
 
 def check_sncl(indata,sn,cl):
-    if (indata.table_highver('AIPS CL')==cl and
-        indata.table_highver('AIPS SN')==sn):
-            print('Tables are fine')
+    if (indata.table_highver("AIPS CL")==cl and
+        indata.table_highver("AIPS SN")==sn):
+            print("Tables are fine")
     
-    if indata.table_highver('AIPS CL')<cl:
-        print(indata.table_highver('AIPS CL'),cl)
-        raise RuntimeError('Not enough CL tables')
+    if indata.table_highver("AIPS CL")<cl:
+        print(indata.table_highver("AIPS CL"),cl)
+        raise RuntimeError("Not enough CL tables")
 
-    if indata.table_highver('AIPS CL')>cl:
-        while indata.table_highver('AIPS CL')>cl:
-            indata.zap_table('AIPS CL', 0)
+    if indata.table_highver("AIPS CL")>cl:
+        while indata.table_highver("AIPS CL")>cl:
+            indata.zap_table("AIPS CL", 0)
 
-    if indata.table_highver('AIPS SN')<sn:
-        print(indata.table_highver('AIPS SN'),sn)
-        raise RuntimeError('Not enough SN tables')
+    if indata.table_highver("AIPS SN")<sn:
+        print(indata.table_highver("AIPS SN"),sn)
+        raise RuntimeError("Not enough SN tables")
 
-    if indata.table_highver('AIPS SN')>sn:
-        while indata.table_highver('AIPS SN')>sn:
-            indata.zap_table('AIPS SN', 0)
+    if indata.table_highver("AIPS SN")>sn:
+        while indata.table_highver("AIPS SN")>sn:
+            indata.zap_table("AIPS SN", 0)
 
 
 ###################################################
@@ -241,14 +241,14 @@ def check_sncl(indata,sn,cl):
 def get_file(path):
     #opens and external file and makes it into a list
     fopen = path
-    f=open(fopen, 'r+')
+    f=open(fopen, "r+")
     g=list(map(lambda s: s.strip(), list(f)))
     return np.array(g)
 
 ###################################################
 ###################################################
 
-if __name__=='__main__':
+if __name__=="__main__":
     main()
 
 ###################################################
