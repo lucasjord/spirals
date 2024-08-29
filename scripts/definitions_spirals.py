cee711e3e76e6d6de6a9356652fe3e48f01a48f4
Lucas Hyland <lucas.jordan.hyland@gmail.com>
2024-08-29 10:05:48 +1000

    Changed: runcalib to run on specific channel

--- scripts/definitions_spirals.py
+++ scripts/definitions_spirals.py
@@ -5016,6 +5016,7 @@ def runcalib(indata,sources=[''],gainuse=0,docal=-1,snver=0,solmode='',soltype='
     calib.aparm[3]    = 1
     calib.aparm[7]    = aparm7
     calib.refant      = refant
+    calib.ichansel[1:]= [chan,chan,0]
     #calib.inp()
     calib()
 
